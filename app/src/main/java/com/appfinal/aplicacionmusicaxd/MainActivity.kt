f1f189500f63e2651e7b3d9bc3f37458ca733c5a
Antony Salcedo <salcedoantony007@gmail.com>
2022-11-19 20:07:22 -0500

    Commit - Archivo Original

---
+++ app/src/main/java/com/appfinal/aplicacionmusicaxd/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.appfinal.aplicacionmusicaxd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_AplicacionMusicaXD)
+        setContentView(R.layout.activity_main)
+    }
+}
