9037538987a6492be6e830b3cff17d84b33c2222
Antony Salcedo <salcedoantony007@gmail.com>
2022-11-19 21:16:44 -0500

    Commit - Antony Salcedo - Agregando las pantallas de favoritos y de listas.

---
+++ app/src/main/java/com/appfinal/aplicacionmusicaxd/PantallaDeListas.kt
@@ -0,0 +1,11 @@
+package com.appfinal.aplicacionmusicaxd
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PantallaDeListas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_de_listas)
+    }
+}
