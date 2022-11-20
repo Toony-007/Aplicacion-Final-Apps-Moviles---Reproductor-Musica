68a16b55ba16146204d2cd3e833803be662f2709
Antony Salcedo <salcedoantony007@gmail.com>
2022-11-19 23:08:42 -0500

    Commit - Antony Salcedo - Agregando las primeras funciones de los botones en la pantalla de inicio de la aplicacion.

--- app/src/main/java/com/appfinal/aplicacionmusicaxd/MainActivity.kt
+++ app/src/main/java/com/appfinal/aplicacionmusicaxd/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.appfinal.aplicacionmusicaxd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.appfinal.aplicacionmusicaxd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Creacndo primer objeto obligatorio
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_AplicacionMusicaXD)
-        setContentView(R.layout.activity_main)
+
+        // Inicializando el objeto binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.botonAleatorio.setOnClickListener{
+            //Toast.makeText(this@MainActivity, "Boton de aleatorio Presionado", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, PantallaDeReproduccion::class.java)
+            startActivity(intent)
+        }
+
+        binding.botonFavoritos.setOnClickListener{
+            val intent = Intent(this@MainActivity, PantallaDeFavoritos::class.java)
+            startActivity(intent)
+        }
+
+        binding.botonListas.setOnClickListener{
+            val intent = Intent(this@MainActivity, PantallaDeListas::class.java)
+            startActivity(intent)
+        }
     }
 }
