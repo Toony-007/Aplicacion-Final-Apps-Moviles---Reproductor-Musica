a872036dd38c923802536c3b7ad6b764b54ce4b7
Antony Salcedo <salcedoantony007@gmail.com>
2022-11-20 21:46:18 -0500

    Commit - Antony Salcedo - Agregando nueva función dentro de la clase data class para calcular la duración de las canciones, además de modificar la clase adaptadorDeMusica creando una nueva variable val root = biding.root, para asignarla a la función onBindViewHolder.

--- app/src/main/java/com/appfinal/aplicacionmusicaxd/AdaptadorDeMusica.kt
+++ app/src/main/java/com/appfinal/aplicacionmusicaxd/AdaptadorDeMusica.kt
@@ -1,8 +1,10 @@
 package com.appfinal.aplicacionmusicaxd
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.appfinal.aplicacionmusicaxd.databinding.ActivityVistaDeLasCancionesBinding
 import com.bumptech.glide.Glide
@@ -15,6 +17,7 @@ class AdaptadorDeMusica(private val contexto: Context, private val listaDeCancio
         val album = biding.idAlbumCancion
         val imagen = biding.imgCancion
         val duracion = biding.idDuracionCancion
+        val root = biding.root
     }
 
     override fun onCreateViewHolder (parent: ViewGroup, viewType: Int): AdaptadorDeMusica.MiTitular
@@ -26,15 +29,17 @@ class AdaptadorDeMusica(private val contexto: Context, private val listaDeCancio
     {
         holder.titulo.text = listaDeCanciones[position].titulo
         holder.album.text = listaDeCanciones[position].album
-        holder.duracion.text = listaDeCanciones[position].duracion.toString()
+        holder.duracion.text = duracionDelFormato(listaDeCanciones[position].duracion)
 
         // Obtener y mostrar la imagen de la cacion
         Glide.with(contexto)
             .load(listaDeCanciones[position].artUri)
             .apply(RequestOptions().placeholder(R.drawable.reproductor_musica_splash_screen).centerCrop())
             .into(holder.imagen)
-
-
+        holder.root.setOnClickListener{
+            val intent = Intent(contexto, PantallaDeReproduccion::class.java)
+            ContextCompat.startActivity(contexto, intent, null)
+        }
     }
 
     override fun getItemCount(): Int
